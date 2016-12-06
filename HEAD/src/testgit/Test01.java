c8fba645ec7cac416fcba6ea8e835c420023fa8b
ventten <790407047@qq.com>
2016-12-06 11:27:08 +0800

    cehsdi

---
+++ HEAD/src/testgit/Test01.java
@@ -0,0 +1,10 @@
+package testgit;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test01");
+	}
+
+}
---
+++ HEAD/src/testgit/Test02.java
@@ -0,0 +1,10 @@
+package testgit;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test02");
+	}
+
+}
