588cdbab43549e8f84a2d686f86776ab8110f169
pangsx <pangsx@uf-club.com>
2017-04-05 18:33:20 +0800

    ...

--- HEAD/src/testgit/Test02.java
+++ HEAD/src/testgit/Test02.java
@@ -4,7 +4,7 @@ public class Test02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test02忽略提交");
+		System.out.println("test02忽略提交---");
 	}
 
 }
