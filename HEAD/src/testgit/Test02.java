2df40535d6096bd80750beb641d8e0a15248fc01
pangsx <pangsx@uf-club.com>
2017-04-11 16:47:32 +0800

    测试

--- HEAD/src/testgit/Test02.java
+++ HEAD/src/testgit/Test02.java
@@ -3,8 +3,9 @@ package testgit;
 public class Test02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("test02忽略提交---");
+		System.out.println("test02测试撤回提交------");
+
 	}
 
 }
