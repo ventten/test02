0fd66b41c92ceb71a273d6c00a69b5a59944dcb4
pangsx <pangsx@uf-club.com>
2017-04-11 16:55:50 +0800

    test

--- HEAD/src/testgit/Test02.java
+++ HEAD/src/testgit/Test02.java
@@ -5,6 +5,8 @@ public class Test02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test02忽略提交---");
+		System.out.println("test02还原到上个版本---");
+
 	}
 
 }
